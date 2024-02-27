1e7b73b5abfd01f591d4c980db82e8a770a50f5a
Dennohkamash <dennohkamash134@gmail.com>
2024-02-27 21:16:22 +0300

    food menu

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,75 @@ void main() {
 class FoodItem {
   final String category;
   final String name;
+  final String imageUrl;
 
-  FoodItem({required this.category, required this.name});
+  FoodItem(
+      {required this.category, required this.name, required this.imageUrl});
 }
 
 List<FoodItem> breakfastItems = [
-  FoodItem(category: 'Breakfast', name: 'Eggs and Bacon'),
-  FoodItem(category: 'Breakfast', name: 'Pancakes'),
+  FoodItem(
+      category: 'Breakfast',
+      name: 'Eggs and Bacon',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
+  FoodItem(
+      category: 'Breakfast',
+      name: 'Pancakes',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
 ];
 
 List<FoodItem> lunchItems = [
-  FoodItem(category: 'Lunch', name: 'Club Sandwich'),
-  FoodItem(category: 'Lunch', name: 'Caesar Salad'),
+  FoodItem(
+      category: 'Lunch',
+      name: 'Club Sandwich',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
+  FoodItem(
+      category: 'Lunch',
+      name: 'Caesar Salad',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
 ];
 
 List<FoodItem> snackItems = [
-  FoodItem(category: 'Snack', name: 'Fruit Salad'),
-  FoodItem(category: 'Snack', name: 'Veggie Sticks'),
+  FoodItem(
+      category: 'Snack',
+      name: 'Fruit Salad',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
+  FoodItem(
+      category: 'Snack',
+      name: 'Veggie Sticks',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
 ];
 
 List<FoodItem> dinnerItems = [
-  FoodItem(category: 'Dinner', name: 'Spaghetti Bolognese'),
-  FoodItem(category: 'Dinner', name: 'Grilled Chicken'),
+  FoodItem(
+      category: 'Dinner',
+      name: 'Spaghetti Bolognese',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
+  FoodItem(
+      category: 'Dinner',
+      name: 'Grilled Chicken',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
 ];
 
 List<FoodItem> beverageItems = [
-  FoodItem(category: 'Beverages', name: 'Coffee'),
-  FoodItem(category: 'Beverages', name: 'Tea'),
+  FoodItem(
+      category: 'Beverages',
+      name: 'Coffee',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
+  FoodItem(
+      category: 'Beverages',
+      name: 'Tea',
+      imageUrl:
+          "https://lowcarbinspirations.com/wp-content/uploads/2021/08/Classic-Bacon-and-Eggs-Recipe-3.jpg"),
 ];
 
 class MyApp extends StatelessWidget {
@@ -55,14 +97,25 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Food Menu'),
-      ),
+          title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.restaurant_menu),
+          SizedBox(
+            width: 10,
+          ),
+          Text("Food Menu")
+        ],
+      )),
       body: Stack(
         children: [
           Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: NetworkImage('https://www.pxfuel.com/en/free-photo-eeedf'),
+                image: NetworkImage(
+                    'https://i.pinimg.com/736x/96/7d/b6/967db683e191acc49969bfd49a0c1056.jpg'),
                 fit: BoxFit.cover,
               ),
             ),
@@ -101,7 +154,10 @@ class FoodCategoriesList extends StatelessWidget {
             category = '';
         }
         return ListTile(
-          title: Text(category),
+          title: Text(
+            category,
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
           onTap: () {
             Navigator.push(
               context,
@@ -116,15 +172,21 @@ class FoodCategoriesList extends StatelessWidget {
   }
 }
 
-class FoodItemsScreen extends StatelessWidget {
+class FoodItemsScreen extends StatefulWidget {
   final String category;
 
   FoodItemsScreen({required this.category});
+  @override
+  _FoodItemsScreenState createState() => _FoodItemsScreenState();
+}
+
+class _FoodItemsScreenState extends State<FoodItemsScreen> {
+  Map<String?, bool> showImageMap = {};
 
   @override
   Widget build(BuildContext context) {
     List<FoodItem> items;
-    switch (category) {
+    switch (widget.category) {
       case 'Breakfast':
         items = breakfastItems;
         break;
@@ -145,15 +207,36 @@ class FoodItemsScreen extends StatelessWidget {
     }
     return Scaffold(
       appBar: AppBar(
-        title: Text(category),
+        title: Text(widget.category),
       ),
       body: Center(
         child: ListView.builder(
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
-            return ListTile(
-              title: Text(item.name),
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: Text(item.name),
+                  onTap: () {
+                    setState(() {
+                      showImageMap[item.name] =
+                          !(showImageMap[item.name] ?? false);
+                    });
+                  },
+                ),
+                if (showImageMap[item.name] ?? false)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network(
+                      item.imageUrl,
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+              ],
             );
           },
         ),
